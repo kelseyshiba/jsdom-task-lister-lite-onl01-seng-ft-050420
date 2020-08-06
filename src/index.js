ee3176d4772908124540698754172a97492f26d8
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-08-05 19:59:42 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let desc = document.querySelector("#new-task-description");
+  let form = document.querySelector("#create-task-form");
+  form.addEventListener("submit", function(e) {
+    const li = document.createElement('li');
+    const button = document.createElement('button')
+    button.innerText = "X"
+    button.id = "del-item"
+    let task = document.querySelector("#tasks").appendChild(li);
+    task.innerHTML = `${desc.value}`;
+    task.appendChild(button);
+    e.preventDefault();
+  });
+
+  // let complete = document.querySelector('#del-item');
+  // complete.addEventListener("click", function(e) {
+  //   complete.parentElement.remove();
+  // });
+
+  let complete = document.querySelector('#tasks');
+  complete.addEventListener("click", function(e) {
+    complete.querySelector('li').remove();
+  });
 });
+
+
